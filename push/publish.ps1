88c28e73d6e18c0285ada7f07eded278e5023bd6
WitherZuo <openworld189@gmail.com>
2023-05-07 23:49:59 +0800

    & c:/Users/withe/Documents/jupyter/Scripts/Activate.ps1

--- push/publish.ps1
+++ push/publish.ps1
@@ -1,12 +1,20 @@
+# Get current date and time
 $current_datetime = Get-Date -UFormat "%Y%m%d-%H%M%S"
 
+# Input commit text
 Write-Host $current_datetime
 $commit = Read-Host "PLEASE INPUT COMMIT TEXT"
 Write-Output $commit
 
+# Check files that will be uploaded
 git status
 git add -A
 git status
+
+# Commit these files to origin/master
 git commit -m "$commit"
+git push origin master
+
+# Tag and upload this tag
 git tag -a NewsPhoto-$current_datetime -m "NewsPhoto: $current_datetime"
 git push origin NewsPhoto-$current_datetime
