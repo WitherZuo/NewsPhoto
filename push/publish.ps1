63df62bd25ff8efff29864fdbfd28bc2db882b2e
WitherZuo <openworld189@gmail.com>
2023-05-07 23:25:41 +0800

    & c:/Users/withe/Documents/jupyter/Scripts/Activate.ps1

--- push/publish.ps1
+++ push/publish.ps1
@@ -1,10 +1,11 @@
 $current_datetime = Get-Date -UFormat "%Y%m%d-%H%M%S"
 
 Write-Host $current_datetime
+$commit = Read-Host "PLEASE INPUT COMMIT TEXT: "
 
 git status
 git add -A
 git status
-git commit -m "Update NewsPhoto."
+git commit -m "$commit"
 git tag -a NewsPhoto-$current_datetime -m "NewsPhoto: $current_datetime"
 git push origin NewsPhoto-$current_datetime
