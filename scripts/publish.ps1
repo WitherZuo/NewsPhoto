ed32a9a6294c3dedc6cb1dfe77bbf183eb9ea4f2
WitherZuo <openworld189@gmail.com>
2024-03-14 09:09:03 +0800

    update publish script.

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,9 +1,9 @@
 # Get current date and time
-$current_datetime = Get-Date -UFormat "%Y%m%d-%H%M%S"
+$current_datetime = Get-Date -UFormat '%Y%m%d-%H%M%S'
 
 # Input commit text
 Write-Host $current_datetime
-$commit = Read-Host "PLEASE ENTER COMMIT INFO"
+$commit = Read-Host 'PLEASE ENTER COMMIT INFO'
 Write-Output $commit
 
 # Check files that will be uploaded
@@ -11,10 +11,31 @@ git status
 git add -A
 git status
 
-# Commit these files to origin/master
-git commit -m "$commit"
-git push origin master
+# Ask question
+$answer = Read-Host 'Do you want to sign commits and tags with GPG key? [Y/n]'
 
-# Tag and upload this tag
-git tag -a NewsPhoto-$current_datetime -m "NewsPhoto: $current_datetime"
+if ($answer -eq 'Y' -or $answer -eq 'y') {
+    Write-Host "Signing commits and tags with GPG key."
+
+    # Commit these files to origin/master, and create a new tag, with key.
+    git commit -S -m "$commit"
+    git tag -s -a NewsPhoto-$current_datetime -m "NewsPhoto: $current_datetime"
+}
+elseif ($answer -eq 'N' -or $answer -eq 'n') {
+    Write-Host "Don't sign commits and tags with GPG key."
+
+    # Commit these files to origin/master, and create a new tag.
+    git commit -m "$commit"
+    git tag -a NewsPhoto-$current_datetime -m "NewsPhoto: $current_datetime"
+}
+else {
+    Write-Host "Invalid input value, dont't sign anything."
+
+    # Commit these files to origin/master, and create a new tag.
+    git commit -m "$commit"
+    git tag -a NewsPhoto-$current_datetime -m "NewsPhoto: $current_datetime"
+}
+
+# Upload all commits and tags
+git push origin master
 git push origin NewsPhoto-$current_datetime
