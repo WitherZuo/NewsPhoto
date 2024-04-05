9f83b50ebed1b722a1a88b058f5ceabf2e021f04
WitherZuo <openworld189@gmail.com>
2024-04-05 13:29:30 +0800

    remove pandoc template due to useless.

--- scripts/preview.ps1
+++ scripts/preview.ps1
@@ -1,3 +1,3 @@
 python .\main.py && `
-pandoc --metadata title='NewsPhoto' --embed-resources --standalone --template='template/newsphoto.html5' --css sources/styles/index.css sources/index.md --output sources/index.html && `
+pandoc --metadata title='NewsPhoto' --embed-resources --standalone --css sources/styles/index.css sources/index.md --output outputs/index.html && `
 python .\browser-autotest.py
