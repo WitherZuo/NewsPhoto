5d694bfda7f00d6f0a4304fbd4380fee25a4d962
WitherZuo <openworld189@gmail.com>
2024-04-06 13:05:20 +0800

    rename save-as-image.py

--- scripts/preview.ps1
+++ scripts/preview.ps1
@@ -1,3 +1,3 @@
 python .\main.py && `
 pandoc --metadata title='NewsPhoto' --embed-resources --standalone --css sources/styles/index.css sources/index.md --output outputs/index.html && `
-python .\browser-autotest.py
+python .\save-as-image.py
