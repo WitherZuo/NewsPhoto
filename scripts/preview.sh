616dbfc4d63e18f32a2e28ea24a7a9ca23989f45
WitherZuo <openworld189@gmail.com>
2024-10-17 21:44:32 +0800

    add newline to every action, and convert CRLF to LF.

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 echo "NewsPhoto 交互式生成"
-echo "欢迎使用 NewsPhoto 交互式生成，可按 CTRL+C 在任意步骤退出。"
+echo -e "欢迎使用 NewsPhoto 交互式生成，可按 CTRL+C 在任意步骤退出。\n"
 
 # 函数，获取用户输入文件路径
 get_news_file() {
     read -p "请输入文件路径 (默认值为 ./news.txt): " news_file
     if [ -z "$news_file" ]; then news_file="./news.txt"; fi
-    echo "文件路径是: $news_file"
+    echo -e "文件路径: $news_file \n"
 }
 
 # 函数，获取用户输入的问候语
 get_greeting_text() {
     read -p "请输入问候语 (默认值为 ……): " greeting_text
     if [ -z "$greeting_text" ]; then greeting_text="……"; fi
-    echo "问候语是: $greeting_text"
+    echo -e "问候语: $greeting_text \n"
 }
 
 # 函数，提示用户选择主题
 choose_theme() {
     while true; do
         echo "请选择一个主题："
-        echo "[L] 浅色 / Light"
-        echo "[D] 深色 / Dark"
-        echo "[S] 春节 / Spring Festival"
+        echo "- [L] 浅色 / Light"
+        echo "- [D] 深色 / Dark"
+        echo "- [S] 春节 / Spring Festival"
 
         # 提示用户输入第一个字母
-        read -p "请输入对应选项的第一个字母（L/D/S）: " choice
+        read -p "请输入对应选项的字母（L/D/S）: " choice
 
         # 将输入转换为小写以便兼容大小写
         choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
 
         case $choice in
-            l) echo "浅色 / Light 主题"; theme="light"; break ;;
-            d) echo "深色 / Dark 主题"; theme="dark"; break ;;
-            s) echo "春节 / Spring Festival 主题"; theme="springfestival"; break ;;
-            *) echo "输入无效，请重试。" ;;
+            l) echo -e "浅色 / Light \n"; theme="light"; break ;;
+            d) echo -e "深色 / Dark \n"; theme="dark"; break ;;
+            s) echo -e "春节 / Spring Festival \n"; theme="springfestival"; break ;;
+            *) echo -e "输入无效，请重试。 \n" ;;
         esac
     done
 }
