4817c8c07ee8380cfad8f86615a31fdbac250b5f
WitherZuo <openworld189@gmail.com>
2025-08-04 20:56:21 +0800

    optimize logic, remove multiple r/w operation.

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ parser = MainArgs.get_parser()
 args = parser.parse_args()
 
 
-# 函数：写入头文件 header.md
+# 函数：生成头部内容
 def write_header_md(
     greeting: str, today_simple_date, today_weekday, today_zhdate
 ):
@@ -42,11 +42,10 @@ def write_header_md(
         f"【 {greeting} 】\n\n",
     ]  # fmt: skip
 
-    with open("sources/header.md", "w", encoding="utf_8") as f:
-        f.writelines(header_text)
+    return header_text
 
 
-# 函数：写入正文文件 content.md
+# 函数：生成正文新闻
 def write_content_md(file_path: str):
     # 处理正文文本 news.txt，写入到新的正文文件：content.md
     with open(file_path, "r", encoding="utf_8") as a:
@@ -54,12 +53,12 @@ def write_content_md(file_path: str):
 
     text_with_space = pangu.spacing(text)
     text_new = re.sub("\n", "  \n\n", text_with_space)
+    content_text = text_new + "\n\n"
 
-    with open("sources/content.md", "w", encoding="utf_8") as b:
-        b.write(text_new + "\n\n")
+    return content_text
 
 
-# 函数：写入底部文件 footer.md
+# 函数：生成底部内容
 def write_footer_md(style, bing_title, today_full_date, timezone):
     # 判断当前使用的样式，决定使用何种样式的二维码
     style_info = style_map.get(style)
@@ -77,8 +76,7 @@ def write_footer_md(style, bing_title, today_full_date, timezone):
         "</footer>",
     ]
 
-    with open("sources/footer.md", "w", encoding="utf_8") as c:
-        c.writelines(footer_text)
+    return footer_text
 
 
 # 函数：使用 Pandoc 导出文档
@@ -99,7 +97,7 @@ def convert_with_pandoc(style):
             "--embed-resources",
             "--standalone",
             f"--css={style_file}",
-            "sources/NewsPhoto.md",
+            "outputs/NewsPhoto.md",
             "--output=outputs/NewsPhoto.html",
         ]
     )  # fmt: skip
@@ -109,10 +107,7 @@ def convert_with_pandoc(style):
 def main():
     # 清理过时文件
     outdated_files = [
-        "sources/header.md",
-        "sources/content.md",
-        "sources/footer.md",
-        "sources/NewsPhoto.md",
+        "outputs/NewsPhoto.md",
         "sources/images/photo.jpg",
         "outputs/NewsPhoto.html",
     ]
@@ -133,14 +128,14 @@ def main():
     Bing.get_bing_image(json_data)
 
     # 生成 NewsPhoto.md 的各个部分
-    write_header_md(
+    header = write_header_md(
         greeting=args.greeting,
         today_simple_date=today_simple_date,
         today_weekday=today_weekday,
         today_zhdate=today_zhdate,
     )
-    write_content_md(file_path=args.news_file)
-    write_footer_md(
+    content = write_content_md(file_path=args.news_file)
+    footer = write_footer_md(
         style=args.style,
         bing_title=bing_title,
         today_full_date=today_full_date,
@@ -148,17 +143,10 @@ def main():
     )
 
     # 汇总后综合写入 NewsPhoto.md
-    with (
-        open("sources/NewsPhoto.md", "a", encoding="utf_8") as newsphoto_md,
-        open("sources/header.md", "r", encoding="utf_8") as header_md,
-        open("sources/content.md", "r", encoding="utf_8") as content_md,
-        open("sources/footer.md", "r", encoding="utf_8") as footer_md,
-    ):
-        header_source = header_md.read()
-        content_source = content_md.read()
-        footer_source = footer_md.read()
-
-        newsphoto_md.write(header_source + content_source + footer_source)
+    with open("outputs/NewsPhoto.md", "a", encoding="utf_8") as newsphoto_md:
+        newsphoto_md.writelines(header)
+        newsphoto_md.write(content)
+        newsphoto_md.writelines(footer)
 
     # 将 Markdown 文档转换为 HTML 格式
     convert_with_pandoc(style=args.style)
